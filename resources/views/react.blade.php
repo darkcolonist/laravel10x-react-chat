158e005e1b2ff7ce472d2f23613f2bec7a990729
cris <cris@nms.ph>
2023-03-14 17:05:34 +0800

    react implementation

---
+++ resources/views/react.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    @vite('resources/js/bootstrap.jsx')
+  </head>
+  <body>
+    <div id="root">
+      javascript must be enabled
+    </div>
+  </body>
+<html>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default ({mode}) => {
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    server: {
+      host: true,
+      hmr: {
+        host: process.env.VITE_DEV_URL,
+      },
+
+      /**
+       * the watch options below should be enabled if working behind a
+       * docker instance
+       */
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      },
+    },
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+      laravel(['resources/js/bootstrap.js']),
+      react(),
     ],
-});
+  });
+}
